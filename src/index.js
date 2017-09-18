c94bab5d871a5235f1fcaf7cd8106b7d83164a71
Douglas Detoni <ddetoni@thoughtworks.com>
2017-09-18 15:48:16 -0300

    Implements basic functions to split bills

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-const hello = () => 'Hello World';
+import R from 'ramda'
 
-export default { hello }
+const splitUpBill = (bill) => {
+  const splitedAmount = parseFloat((bill.amount / bill.group.length).toFixed(1))
+
+  return R
+    .filter(member => member !== bill.owner, bill.group)
+    .map(member => {
+      return {
+        from: member,
+        to: bill.owner,
+        amount: splitedAmount
+      }
+    })
+}
+
+const splits = (bills) => {
+  return R
+    .flatten(R
+      .map(splitUpBill, bills))
+}
+
+export default { splitUpBill, splits }
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,57 @@
 import lannister from '../src/index';
 
-test('retuns hello world', () => {
-  expect(lannister.hello()).toBe('Hello World');
+const firstBill = {
+  owner: 'ddetoni',
+  group: ['ddetoni', 'piperfe', 'machuca'],
+  amount: 29710
+}
+
+const secondBill = {
+  owner: 'piperfe',
+  group: ['ddetoni', 'piperfe', 'machuca'],
+  amount: 15632
+}
+
+test('splits just one bill', () => {
+  const expectedResponse = [
+    {
+      from: 'piperfe',
+      to: 'ddetoni',
+      amount: 9903.3
+    },
+    {
+      from: 'machuca',
+      to: 'ddetoni',
+      amount: 9903.3
+    }
+  ]
+
+  expect(lannister.splitUpBill(firstBill)).toEqual(expectedResponse);
+});
+
+test('splits multiple bills', () => {
+  const expectedResponse = [
+    {
+      from: 'piperfe',
+      to: 'ddetoni',
+      amount: 9903.3
+    },
+    {
+      from: 'machuca',
+      to: 'ddetoni',
+      amount: 9903.3
+    },
+    {
+      from: 'ddetoni',
+      to: 'piperfe',
+      amount: 5210.7
+    },
+    {
+      from: 'machuca',
+      to: 'piperfe',
+      amount: 5210.7
+    }
+  ]
+
+  expect(lannister.splits([firstBill, secondBill])).toEqual(expectedResponse);
 });
